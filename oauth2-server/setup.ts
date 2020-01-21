bf55db0d9cb0c964539b5d2d39ddd3240693fe55
Teodor Bozhikov <t_bozhikov@yahoo.com>
2020-01-21 17:34:34 +0200

    verify token sent by client

--- oauth2-server/setup.ts
+++ oauth2-server/setup.ts
@@ -4,35 +4,48 @@ import * as crypto from 'crypto';
 import * as express from 'express';
 import { MongoClient } from 'mongodb';
 
-console.log(process.env.MONGO_INITDB_ROOT_USERNAME);
-console.log(process.env.MONGO_INITDB_ROOT_PASSWORD);
-console.log(process.env.DOCKER);
-
 const jwt = require('jsonwebtoken');
 const url = `mongodb://admin-user:admin-password@${process.env.DOCKER ? 'mongodb' : 'localhost'}:27017/myOauth2`;
-console.log(url);
-const users = [
-    { id: 1, username: 'toto', password: 'secret', displayName: 'Toto', emails: [{ value: 'toto@toto.com' }] }
-    , { id: 2, username: 'kiro', password: 'kiro', displayName: 'Kiro', emails: [{ value: 'kiro@kiro.com' }] }
-];
+const jwtSecret = 'some-very-secure-secret';
 
-MongoClient.connect(url, (err: any, mongoClient) => {
-    if (err) throw err;
-    var dbo = mongoClient.db("MyOauth2");
-    dbo.collections().then(value => { console.log(value) });
-    // dbo.collection("Users").findOne({}, function (err: any, result: any) {
-    //     if (err) throw err;
-    //     console.log(result.username);
-    //     mongoClient.close();
-    // });
-    mongoClient.close();
-});
+export function setupServer(server: core.Express) {
+    server.use(express.json());
 
-const findByUsername = (username: any, callback: (err: any, user: any) => any) => {
+    server.get('/test-unprotected', (req: any, res: any, next: any) => {
+        res.status(200).send('THIS IS PUBLIC, SHARE FREELY!');
+    });
+
+    server.get('/test-protected', isAuthorized, (req: any, res: any, next: any) => {
+        res.json({ "message": "THIS IS SUPER SECRET, DO NOT SHARE!" });
+    });
+
+    server.post('/users/authenticate',
+        (req: any, res: any, next: any) => {
+            console.log(`authenticating user, encrypted msg: ${req.body.base64EncodedMsg}`);
+            console.log(`req.body: ${JSON.stringify(req.body)}`);
+
+            let decryptedMessage = decrypt(req.body.base64EncodedMsg);
+            console.log(`decrypted msg: ${decryptedMessage}`);
+
+            let user = extractUserDataFromMessage(decryptedMessage);
+
+            findUser(user, (err, userFromStore) => {
+                if (userFromStore) {
+                    // let token = jwt.sign({ 'user': 'stuff' }, 'some-very-secure-secret');
+                    let token = createToken();
+                    res.json({ access_token: token });
+                } else {
+                    res.sendStatus(403);
+                }
+            });
+        });
+}
+
+function findUser(user: any, callback: (err: any, user: any) => any) {
     MongoClient.connect(url, (err: any, mongoClient) => {
         if (err) throw err;
-        var dbo = mongoClient.db("MyOauth2");
-        dbo.collection("Accounts").findOne({ username: username }, function (err: any, result: any) {
+        var dbo = mongoClient.db("myOauth2");
+        dbo.collection("Accounts").findOne({ username: user.username, password: user.password }, function (err: any, result: any) {
             if (err) throw err;
             console.log(JSON.stringify(result));
             mongoClient.close();
@@ -45,66 +58,12 @@ const findByUsername = (username: any, callback: (err: any, user: any) => any) =
         });
     });
 };
-// const findByUsername = (username: any, callback: (err: any, user: any) => any) => {
-//     for (let i = 0, len = users.length; i < len; i++) {
-//         const user = users[i];
-//         if (user.username === username) {
-//             return callback(null, user);
-//         }
-//     }
-//     return callback(null, null);
-// };
 
-export function setupServer(server: core.Express) {
-    server.use(express.json());
-    server.get('/health', (req: any, res: any, next: any) => {
-        res.sendStatus(200);
-    });
-
-    server.get('/test', (req: any, res: any, next: any) => {
-        res.status(200).send('test endpoint works');
-    });
-
-    server.get('/testcheck', isAuthorized, (req: any, res: any, next: any) => {
-        res.json({ "message": "THIS IS SUPER SECRET, DO NOT SHARE!" });
-    });
-
-    server.get('/users/authenticate',
-        (req: any, res: any, next: any) => {
-            let privateKey = fs.readFileSync('./rsa256/jwt-rs256.key', 'utf8');
-            console.log(privateKey);
-            // create token for user
-            let token = jwt.sign({ "body": "stuff" }, privateKey, { algorithm: 'RS256' });
-            console.log(">>> token", token);
-
-
-            // res.status(200).send();
-        });
-
-    server.post('/users/authenticate',
-        (req: any, res: any, next: any) => {
-            console.log(`authenticating user, encrypted msg: ${req.body.base64EncodedMsg}`);
-            console.log(`req.body: ${JSON.stringify(req.body)}`);
-
-            let decryptedMessage = decrypt(req.body.base64EncodedMsg);
-            console.log(`decrypted msg: ${decryptedMessage}`);
-
-            let user = extractUserDataFromMessage(decryptedMessage);
-
-            console.log('searching for user', user)
-
-            findByUsername(user.username, (err, userFromStore) => {
-
-                console.log('searching finished', userFromStore);
-
-                if (userFromStore) {
-                    let token = jwt.sign({ 'user': 'stuff' }, 'some-very-secure-secret');
-                    res.json({ access_token: token });
-                } else {
-                    res.sendStatus(403);
-                }
-            });
-        });
+function createToken(): any {
+    return jwt.sign({
+        'exp': Math.floor(Date.now() / 1000) + (60 * 60),
+        'data': 'bla bla'
+    }, jwtSecret);
 }
 
 function extractUserDataFromMessage(message: string): any {
@@ -121,29 +80,24 @@ function decrypt(base64EncodedMsg: string): string {
 
 function isAuthorized(req: any, res: any, callback: any) {
     if (typeof req.headers.authorization !== "undefined") {
-        // retrieve the authorization header and parse out the
-        // JWT using the split function
         let token = req.headers.authorization.split(" ")[1];
 
-        // console.log("token", token);
-        let publicKey = fs.readFileSync('./rsa256/jwt-rs256.key.pub', 'utf8');
-        // Here we validate that the JSON Web Token is valid and has been 
-        // created using the same private pass phrase
-        jwt.verify(token, publicKey, { algorithms: ['RS256'] }, (err: any, user: any) => {
+        // validate that the token
+        jwt.verify(token, jwtSecret, (err: any, decoded: any) => {
+            // TODO: perform additional checks on "decoded" to validate expirations, etc. 
 
             // if there has been an error...
             if (err) {
                 console.log(err);
-                // shut them out!
-                res.status(500).json({ error: "Not Authorized" });
+
+                res.status(403).json({ error: "Not Authorized" });
                 throw new Error("Not Authorized");
             }
-            // if the JWT is valid, allow them in
+
             return callback();
         });
     } else {
-        // No authorization header exists on the incoming
-        // request, return not authorized and throw a new error 
+        // if no Authorization header, return not authorized and throw a new error 
         res.status(500).json({ error: "Not Authorized" });
         throw new Error("Not Authorized");
     }
